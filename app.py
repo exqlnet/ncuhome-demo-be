34521c37f2dc69cfad4912556e4ac80f6461925b
李聪 <591210216@qq.com>
2018-11-02 19:59:23 +0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 app.config["SQLALCHEMY_DATABASE_URI"] = r"sqlite:///.\demo.db"
 
 db = SQLAlchemy(app)
